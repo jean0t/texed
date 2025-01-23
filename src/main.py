24569fd959d4c1f0fc2bb340fc9eadcc5c09ff31
jean0t <joaomouryf@hotmail.com>
2025-01-22 21:59:25 -0300

    fix: small changes to avoid error messages

--- src/main.py
+++ src/main.py
@@ -4,16 +4,19 @@ from sys import argv
 from datetime import datetime
 
 def main():
-    app = App()
-    if len(argv) < 2:
-        now = datetime.now()
-        format_date = now.strftime("%Y/%m/%d_%H:%M")
-        app.set_path(format_date + ".txt")
-    else:
-        app.set_path(argv[1])
+    try:
+        app = App()
+        if len(argv) < 2:
+            now = datetime.now()
+            format_date = now.strftime("%Y/%m/%d_%H:%M")
+            app.set_path(format_date + ".txt")
+        else:
+            app.set_path(argv[1])
 
-    app.configure()
-    app.run()
+        app.configure()
+        app.run()
+    except:
+        pass
 
 if __name__ == "__main__":
     main()
